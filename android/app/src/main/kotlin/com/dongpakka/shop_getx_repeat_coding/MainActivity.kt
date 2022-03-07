e4474c92e1843c5db484f05ae7cea01330b0b05c
semidongdong <semidongdong@gmail.com>
2022-03-07 22:08:23 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/dongpakka/shop_getx_repeat_coding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dongpakka.shop_getx_repeat_coding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shop_getx_repeat_coding/app/ui/page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
